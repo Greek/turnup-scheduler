ae90f2c63f32d4df557e8d1d05df748ab0eae174
Andreas <git@fsck.rip>
2025-08-06 17:23:38 -0700

    clearer logging
    Signed-off-by: Andreas <git@fsck.rip>

--- internal/env/loader.go
+++ internal/env/loader.go
@@ -11,12 +11,13 @@ import (
 func LoadEnv() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Print("Error loading .env file... Continuing env check.")
+		log.Print("env file does not exist")
 	}
 }
 
 // CheckEnv checks if the proper environment variables are present
 func CheckEnv() {
+	log.Print("checking env variables")
 	val := os.Getenv("REDIS_URL")
 	if len(val) == 0 {
 		log.Fatal("REDIS_URL is not defined, exiting")
