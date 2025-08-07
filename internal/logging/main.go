03e10e9e7f16c56fba1ddb57db59a49abc66cadd
Andreas <git@fsck.rip>
2025-08-06 22:00:00 -0700

    chore(logging): make logger level debug if not prod
    Signed-off-by: Andreas <git@fsck.rip>

--- internal/logging/main.go
+++ internal/logging/main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func BuildLogger(method string) *slog.Logger {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{})).With("method", method)
+	level := slog.LevelInfo
+	if os.Getenv("ENV") != "prod" {
+		level = slog.LevelDebug
+	} else {
+		level = slog.LevelError
+	}
 
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level})).With("method", method)
 	return logger
 }
